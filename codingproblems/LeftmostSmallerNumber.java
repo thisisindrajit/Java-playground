8ccb61183c5f89af5cd85988f0adf11a8afcca09
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-02-10 17:26:16 +0530

    Update LeftmostSmallerNumber.java

--- codingproblems/LeftmostSmallerNumber.java
+++ codingproblems/LeftmostSmallerNumber.java
@@ -45,7 +45,6 @@ import java.util.Arrays;
 public class LeftmostSmallerNumber {
     public static void main(String[] args) {
         Solution sol = new Solution();
-
         System.out.println(Arrays.toString(sol.findLeftmostSmallerNumber(new int[]{ 2, 1, 3, 2, 1, 3 }, 3))); // Should return [-1, -1, 2, 1, -1, 2]
     }
 }
